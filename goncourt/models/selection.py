45cf4564c4d3ba7cc6d02833b1075a0c5e27b307
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-12-09 10:37:21 +0100

    feat: class selection

---
+++ goncourt/models/selection.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
+class Selection:
+    id_selection: Optional[int] = field(default=None, init=False)
+    date: datetime
+    selection_nb: int
+
+    def __str__(self):
+        return f"Selection {self.selection_nb} en date du {self.date} : "
