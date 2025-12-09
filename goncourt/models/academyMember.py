6df9f0868caafcc88a28a49451e5f3a8dd2b50a5
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-12-09 10:34:43 +0100

    feat: class academyMember

--- goncourt/models/academyMember.py
+++ goncourt/models/academyMember.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class AcademyMember:
+    id_academy_member: Optional[int] = field(default=None, init=False)
     is_president: bool
 
     def __str__(self):
